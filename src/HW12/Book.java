b1183a824f6c60b6378d52e7db9adaf9e87d4f5f
nadsay <nadsatyyy@gmail.com>
2023-09-07 18:26:35 +0300

    Домашка 12.1

---
+++ src/HW12/Book.java
@@ -0,0 +1,27 @@
+package HW12;
+
+public class Book {
+    private String bookName;
+    private int bookYear;
+
+    public Book (String bookName, int bookYear) {
+        this.bookName = bookName;
+        this.bookYear = bookYear;
+    }
+
+    public String getBookName () {
+        return this.bookName;
+    }
+
+    public int getBookYear () {
+        return this.bookYear;
+    }
+
+    public void setBookName () {
+        this.bookName = bookName;
+    }
+
+    public void setBookYear (int bookYear) {
+        this.bookYear = bookYear;
+    }
+}
---
+++ src/HW12/Main.java
@@ -0,0 +1,20 @@
+package HW12;
+
+public class Main {
+    public static void main(String[] args) {
+        Book warAndPeace = new Book ("Война и мир", 1886);
+        System.out.println("Книга - " + warAndPeace.getBookName() + ", " + warAndPeace.getBookYear() + " год");
+
+        Author levTolstoy = new Author("Лев", "Толстой");
+        System.out.println("Автор - " + levTolstoy.getAuthorFirstName() + " " + levTolstoy.getAuthorLastName());
+
+        Book idiot = new Book("Бесы", 1872);
+        System.out.println("Книга - " + idiot.getBookName() + ", " + idiot.getBookYear() + " год");
+
+        Author fedorDostoevsky = new Author("Федор", "Достоевский");
+        System.out.println("Автор - " + fedorDostoevsky.getAuthorFirstName() + " " + fedorDostoevsky.getAuthorLastName());
+
+        warAndPeace.setBookYear(1869);
+        System.out.println("\n *поправка: год издания книги 'Война и мир' - " + warAndPeace.getBookYear());
+    }
+}
